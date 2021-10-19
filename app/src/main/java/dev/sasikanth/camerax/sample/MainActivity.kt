d8cd7a84fab9e375adf42fd491fdc0179aefe787
Harish <harish.s.pukale123@gmail.com>
2021-10-19 20:16:47 +0530

    working :)

--- app/src/main/java/dev/sasikanth/camerax/sample/MainActivity.kt
+++ app/src/main/java/dev/sasikanth/camerax/sample/MainActivity.kt
@@ -3,13 +3,25 @@ package dev.sasikanth.camerax.sample
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(OpenCVLoader.initDebug()){
+
+            Log.d("Check","OpenCv configured successfully");
+
+        } else{
+
+            Log.d("Check","OpenCv doesn’t configured successfully");
+
+        }
 
         scanQr.setOnClickListener {
             startActivity(Intent(this, ScanActivity::class.java))
--- app/src/main/java/dev/sasikanth/camerax/sample/QrCodeAnalyzer.kt
+++ app/src/main/java/dev/sasikanth/camerax/sample/QrCodeAnalyzer.kt
@@ -1,5 +1,6 @@
 package dev.sasikanth.camerax.sample
 
+import android.graphics.Bitmap
 import android.graphics.ImageFormat.*
 import android.os.Build
 import android.util.Log
@@ -7,7 +8,16 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.zxing.*
 import com.google.zxing.common.HybridBinarizer
+import org.opencv.android.Utils
+import org.opencv.core.CvType
+import org.opencv.core.Mat
 import java.nio.ByteBuffer
+import org.opencv.core.Core
+
+import org.opencv.imgproc.Imgproc
+
+
+
 
 private fun ByteBuffer.toByteArray(): ByteArray {
     rewind()
@@ -35,6 +45,29 @@ class QrCodeAnalyzer(
         setHints(map)
     }
 
+    private fun ImageProxy.convertYUVtoMat() : Mat{
+        val yBuffer = planes[0].buffer // Y
+        val uBuffer = planes[1].buffer // U
+        val vBuffer = planes[2].buffer // V
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+
+        val yuvImage = Mat(this.height + this.height/2, this.width, CvType.CV_8UC1)
+        yuvImage.put(0,0,nv21)
+        val rgb = Mat()
+        Imgproc.cvtColor(yuvImage, rgb, Imgproc.COLOR_YUV2RGB_NV21, 3)
+        Core.rotate(rgb, rgb, Core.ROTATE_90_CLOCKWISE)
+        return rgb
+    }
+
     override fun analyze(image: ImageProxy) {
         // We are using YUV format because, ImageProxy internally uses ImageReader to get the image
         // by default ImageReader uses YUV format unless changed.
@@ -43,18 +76,39 @@ class QrCodeAnalyzer(
             return
         }
 
-        val data = image.planes[0].buffer.toByteArray()
+//        val data = image.planes[0].buffer.toByteArray()
+//
+//        val source = PlanarYUVLuminanceSource(
+//            data,
+//            image.width,
+//            image.height,
+//            0,
+//            0,
+//            image.width,
+//            image.height,
+//            false
+//        )
 
-        val source = PlanarYUVLuminanceSource(
-            data,
-            image.width,
-            image.height,
-            0,
-            0,
-            image.width,
-            image.height,
-            false
-        )
+        val src = image.convertYUVtoMat()
+        val gray = Mat()
+
+        Imgproc.cvtColor(src,gray,Imgproc.COLOR_RGB2GRAY)
+
+        // destination has grayscale
+
+        val final = Mat()
+
+        Core.bitwise_not(gray,final)
+
+        val bmp = Bitmap.createBitmap(final.width(),final.height(),Bitmap.Config.RGB_565)
+        Utils.matToBitmap(final,bmp)
+
+        val intArray = IntArray(bmp.width * bmp.height)
+        bmp.getPixels(intArray, 0, bmp.width, 0, 0, bmp.width, bmp.height)
+
+        val source: LuminanceSource = RGBLuminanceSource(bmp.width, bmp.height, intArray)
+
+
 
         val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
         try {
